09dac58059e02da0d00716d482c7f3a93e9915c1
Derek Paiva <142218247+derekpaiva@users.noreply.github.com>
2023-09-03 01:05:15 -0300

    Adicionei 10 novos exercícios

---
+++ exe003.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite mais um número: '))
+s = int(n1 + n2)
+print('A soma vale', s)
---
+++ exe005.py
@@ -0,0 +1,2 @@
+n = int(input('Digite um número: '))
+print('Analisando o número {}, seu sucessor é o {} \n Seu antecessor é o {}'.format((n), (n+1), (n-1)))
