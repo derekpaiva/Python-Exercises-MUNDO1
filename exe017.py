36ec516c3c97e7527ef9b8d88fb86417dd1d4825
Derek Paiva <142218247+derekpaiva@users.noreply.github.com>
2023-09-05 08:44:02 -0300

    Novo update

--- exe017.py
+++ exe017.py
@@ -1,8 +1,5 @@
 import math
-from math import pow, sqrt
-
 cato = float(input('Digite o valor do cateto oposto do triângulo retângulo: '))
 cata = float(input('Digite o valor do cateto adjacente do triângulo retângulo: '))
-h = cato**2 + cata**2
-hip = math.sqrt(h)
+hip = math.sqrt(math.pow(cato,2)+math.pow(cata,2))
 print('O valor da hipotenusa desse triângulo retângulo é {:.2f}'. format(hip))
