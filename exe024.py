e707b9ef81f1f0f786236e9c1cbb226095de80ee
Derek Paiva <142218247+derekpaiva@users.noreply.github.com>
2023-09-08 23:10:22 -0300

    Update exe024.py

--- exe024.py
+++ exe024.py
@@ -1,6 +1,7 @@
-city = str(input('Digite o nome de sua cidade: '))
-city_div = city.split()
+city = str(input('Digite o nome de sua cidade: ')).strip()
+city2 = city.title()
+city_div = city2.split()
 
-print('A cidade {} começa com o nome Santo?'.format(city), end=' ')
+print('A cidade {} começa com o nome Santo?'.format(city2), end=' ')
 # o comando 'Santo' in city_div[0] procura na primeira palavra se tem Santo
 print('Santo' in city_div[0])
